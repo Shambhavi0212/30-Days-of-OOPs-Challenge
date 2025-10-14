1495c890802c812d868bbf17bb49d11c4f8effe4
Vallapi Shambhavi <vallapishambhavi02@gmail.com>
2025-10-14 22:52:40 +0530

    Day 24/30: Added 'Java Errors' content

---
+++ Day 24 - Java Errors/logical_error.java
@@ -0,0 +1,10 @@
+
+public class logical_error {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a - b;
+        System.out.println(sum);
+    }
+}
---
+++ Day 24 - Java Errors/run_time.java
@@ -0,0 +1,9 @@
+
+public class run_time {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        System.out.println(a / b);
+    }
+}
