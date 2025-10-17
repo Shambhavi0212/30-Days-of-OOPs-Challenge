da5156bf7d9bfb7728e9a9e9859966a936c719de
Vallapi Shambhavi <vallapishambhavi02@gmail.com>
2025-10-17 23:02:50 +0530

    Day 25/30: Added 'Java Debuggings' content

---
+++ Day 25 - Java Debuggings/debug.java
@@ -0,0 +1,11 @@
+
+public class debug {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        System.out.println("Before Division ");
+        int result = x / y;
+        System.out.println("Result " + result);
+    }
+}
